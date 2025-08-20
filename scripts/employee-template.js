2023634e98b5093cdfd1485d12ec8c7d670b6965
Lýz <147095077+Lyzurly@users.noreply.github.com>
2025-08-19 21:30:21 -0500

    employees are hirable

--- scripts/employee-template.js
+++ scripts/employee-template.js
@@ -1,44 +1,44 @@
-const employee_template = `
-        <div class="employee-container employee1">
-            <div class="abilities-container">
-                <span class="button-wrapper">
-                    <button class="text-mdlg" id="button1">
-                        Pay
-                    </button>
-                    <span class="text-sm popup reveal">
-                        Wages +1
-                    </span>
+ const employee_template =`
+    <div class="employee-container" id="employee">
+        <div class="abilities-container">
+            <span class="button-wrapper">
+                <button class="text-mdlg button-pay">
+                    Pay
+                </button>
+                <span class="text-sm popup reveal">
+                    Wages +1
                 </span>
-                <span class="button-wrapper">
-                    <button class="text-mdlg" id="button2">
-                        Praise
-                    </button>
-                    <span class="text-sm popup reveal">
-                        Happiness +1
-                    </span>
+            </span>
+            <span class="button-wrapper">
+                <button class="text-mdlg button-praise">
+                    Praise
+                </button>
+                <span class="text-sm popup reveal">
+                    Happiness +1
                 </span>
-            </div>
-
-            <div class="stat-container">
-                <span class="title-md stat-wrapper">
-                    INCOME
-                    <div class="text-lg stat-value" id="stat1">
-                        5
-                    </div>
-                </span>
-                <span class="title-md stat-wrapper">
-                    HAPPINESS
-                    <div class="text-lg stat-value" id="stat2">
-                        5
-                    </div>
-                </span>
-                <span class="title-md stat-wrapper">
-                    WELLBEING
-                    <div class="text-lg overall-value">
-                        10
-                    </div>
-                </span>
-            </div>
+            </span>
         </div>
-        <hr class="employee-separator"></hr>
-        `
+
+        <div class="stat-container">
+            <span class="title-md stat-wrapper">
+                INCOME
+                <div class="text-lg stat-value stat-pay">
+                    5
+                </div>
+            </span>
+            <span class="title-md stat-wrapper">
+                HAPPINESS
+                <div class="text-lg stat-value stat-praise">
+                    5
+                </div>
+            </span>
+            <span class="title-md stat-wrapper">
+                WELLBEING
+                <div class="text-lg overall-value">
+                    10
+                </div>
+            </span>
+        </div>
+    </div>
+    <hr class="employee-separator"></hr>
+    `
--- scripts/stat-management.js
+++ scripts/stat-management.js
@@ -1,58 +1,92 @@
+const employees_canvas = document.querySelector('.employees-canvas')
+let employee_idno_previous = -1
+let employees = []
+const employee_id_prefix = "employee"
+
+
+const button_hire = document.querySelector('#hire')
 
 const max_stat = 10
 const min_stat = 0
 const base_inc_stat = 5
 const base_wellbeing = 10
 
-const stats = document.querySelectorAll('.stat-value')
+button_hire.addEventListener('click', function(event){
+    addNewEmployee()
+})
 
-const employees_canvas = document.querySelector('.employees-canvas')
+class Employee{
+    constructor(
+        employee_id
+    ){
+        this.employee = document.getElementById(employee_id.toString())
+        this.buttons = this.employee.querySelectorAll('button')
+        this.buttons.forEach(button => {
+            button.addEventListener('click', function(event){
+                //increase stat
+                switch (event.target.className) {
+                    case 'button-pay':
+                        stat = this.employee.classList('.stat-pay')
+                        increaseStat(stat)
+                    break;
+                    case 'button-praise':
+                        stat = this.employee.classList('.stat-praise')
+                        increaseStat(stat)
+                    break;
+                    default:
+                        console.log("Unknown abilitiy used.")
+                }
+           })});
 
-addNewEmployee()
+        this.stats = this.employee.querySelectorAll('.stat-value')
+    }
+
+    decreaseStats() {
+        this.stats.forEach(stat => {
+            //decreasing if stat is in range
+            let value = base_inc_stat
+            stat.innerHTML = value
+            if (value > 0) {
+                value--;
+            }
+            stat.innerHTML = value
+        })
+    }
+    
+} 
+
 function addNewEmployee() {
+    employee_idno_current = employee_idno_previous++
+    employee_idno_previous = employee_idno_current
+
+    employee_id = employee_id_prefix + employee_idno_current.toString()
+
     employees_canvas.innerHTML += employee_template
+
+    employee_container = document.getElementById(employee_id_prefix)
+    employee_container.id = employee_id
+
+    employees.push(
+        new Employee(
+            employee_id,
+        )
+    )
 }
-addNewEmployee()
 
-// setInterval(decreaseStats,1000)
-
-function decreaseStats() {
-    stats.forEach(stat => {
-        //decreasing if stat is in range
-        let value = base_inc_stat
-        stat.innerHTML = value
-        if (value > 0) {
-            value--;
-        }
-        stat.innerHTML = value
+setInterval(updateEmployees,1000)
+function updateEmployees(){
+    employees.forEach(employee =>{
+        employee.decreaseStats()
     })
 }
 
-const buttons = document.querySelectorAll('button')
-buttons.forEach(button => {
-    button.addEventListener('click', function(event){
-        //increase stat
-        switch (event.target.id) {
-            case "button1":
-                stat1 = document.getElementById('stat1')
-                value = stat1.innerHTML;
-                if (isInRange(value)) {
-                    value++;
-                }
-                stat1.innerHTML = value
-            break;
-            case "button2":
-                stat1 = document.getElementById('stat2')
-                value = stat1.innerHTML;
-                if (isInRange(value)) {
-                    value++;
-                }
-                stat1.innerHTML = value
-            break;
-            default:
-                console.log("Unknown abilitiy used.")
-        }
-    })});
+function increaseStat(stat){
+    value = stat.innerHTML;
+    if (isInRange(stat_value)) {
+        value++;
+    }
+    stat.innerHTML = value
+}
 
 function isInRange(value) {
     //return if value between min and max
