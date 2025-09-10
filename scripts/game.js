d5a2099cbab3c5bb18de055667ce5ed68a4c5262
Lýz <147095077+Lyzurly@users.noreply.github.com>
2025-09-10 01:39:05 -0500

    profit go up if worker ok

--- scripts/game.js
+++ scripts/game.js
@@ -11,6 +11,7 @@ const class_prefix_stat = "stat-";
 const button_hire_employee = document.querySelector("#hire-employee");
 const button_hire_intern = document.querySelector("#hire-intern");
 const button_hire_test = document.querySelector("#hire-test");
+const value_profit = document.querySelector("#profit");
 
 const max_stat = 10;
 const min_stat = 0;
@@ -19,6 +20,7 @@ const base_wellbeing = 10;
 
 const default_tick_interval = 2;
 const default_tick_counter = default_tick_interval;
+const default_income_tolerance = 5;
 
 const sprite_path = "assets/images/HGDEmployee.gif";
 
@@ -57,8 +59,8 @@ class Worker {
 
   createWorkerInterface(stats) {
     this.worker_interface = create_element("div", "worker-interface");
-    stats.forEach((stat) => {
-      this.worker_interface.appendChild(stat.stat_interface);
+    Object.entries(stats).forEach((stat) => {
+      this.worker_interface.appendChild(stat[1].stat_interface);
     });
     this.worker_container.appendChild(this.worker_interface);
     this.addWorkerToDOM(this.worker_container);
@@ -72,10 +74,10 @@ class Worker {
 class Employee extends Worker {
   constructor(worker_id) {
     super(worker_id);
-    this.stats = [
-      new Income(this.worker_container),
-      new Happiness(this.worker_container),
-    ];
+    this.stats = {
+      income: new Income(this.worker_container, this),
+      happiness: new Happiness(this.worker_container, this),
+    };
 
     this.createWorkerInterface(this.stats);
   }
@@ -84,10 +86,10 @@ class Employee extends Worker {
 class Intern extends Worker {
   constructor(worker_id) {
     super(worker_id);
-    this.stats = [
-      new Happiness(this.worker_container),
-      new Duration(this.worker_container),
-    ];
+    this.stats = {
+      duration: new Duration(this.worker_container, this),
+      happiness: new Happiness(this.worker_container, this),
+    };
 
     this.createWorkerInterface(this.stats);
   }
@@ -96,7 +98,7 @@ class Intern extends Worker {
 class TestWorker extends Worker {
   constructor(worker_id) {
     super(worker_id);
-    this.stats = [new TestStat(this.worker_container)];
+    this.stats = { teststat: new TestStat(this.worker_container, this) };
 
     this.createWorkerInterface(this.stats);
   }
@@ -120,8 +122,9 @@ button_hire_test.addEventListener("click", function (event) {
 
 // ABSTRACT CLASS //
 class Stat {
-  constructor(worker_container) {
+  constructor(worker_container, worker_obj) {
     this.worker_container = worker_container;
+    this.worker_obj = worker_obj;
     this.stat_interface = create_element("div", "stat-interface");
 
     this.tick_counter = default_tick_counter;
@@ -129,16 +132,63 @@ class Stat {
 
   createTickListener(tick_interval = default_tick_interval) {
     this.tick_interval = tick_interval;
+    this.tick_interval_fast = Math.floor(this.tick_interval / 5);
+    this.tick_counter = this.tick_interval;
     window.addEventListener("tick", () => {
       this.tickOnInterval(decreaseStat, this.stat_value, this.tick_interval);
     });
   }
 
-  tickOnInterval(fn_onTick, fn_onTickParam = null) {
+  tickOnInterval(fn_onInterval, fn_onIntervalParam = null) {
     this.tick_counter--;
+    this.monitorOnTick(this.constructor.name);
     if (this.tick_counter <= 0) {
       this.tick_counter = this.tick_interval;
-      fn_onTick(fn_onTickParam);
+      fn_onInterval(fn_onIntervalParam);
+      // this.monitorOnInterval(this.constructor.name);
+    }
+  }
+
+  monitorOnTick(stat_name) {
+    // TODO setup increment in general, start with praise
+
+    // TODO separately readable global profit stat (needs to be implemented good) that updates the innerHTML and can be siphoned from for payment and hiring
+
+    this.latestValue_income = 0;
+    if (this.worker_obj.stats.hasOwnProperty("income")) {
+      this.latestValue_income =
+        this.worker_obj.stats.income.stat_value.innerHTML;
+    }
+
+    this.latestValue_happiness = 0;
+    if (this.worker_obj.stats.hasOwnProperty("happiness")) {
+      this.latestValue_happiness =
+        this.worker_obj.stats.happiness.stat_value.innerHTML;
+    }
+
+    this.currentProfit = value_profit.innerHTML;
+
+    value_profit.innerHTML =
+      Number(this.currentProfit) +
+      Number(this.latestValue_income) +
+      Number(this.latestValue_happiness);
+
+    switch (stat_name) {
+      case "Happiness":
+        this.income_value = fetchClassEleInID(
+          this.worker_container.id,
+          "stat-Income"
+        ).innerHTML;
+        if (this.income_value < default_income_tolerance) {
+          this.tick_interval = this.tick_interval_fast;
+          console.log("Faster interval is ", this.tick_interval);
+          if (this.tick_counter > this.tick_interval) {
+            this.tick_counter = this.tick_interval;
+          }
+        }
+        break;
+      default:
+      // //
     }
   }
 
@@ -182,8 +232,8 @@ class Stat {
 }
 
 class Duration extends Stat {
-  constructor(worker_container) {
-    super(worker_container);
+  constructor(worker_container, worker_obj) {
+    super(worker_container, worker_obj);
     this.stat_value = this.create_stat(this.constructor.name);
 
     this.createTickListener();
@@ -191,8 +241,8 @@ class Duration extends Stat {
 }
 
 class Income extends Stat {
-  constructor(worker_container) {
-    super(worker_container);
+  constructor(worker_container, worker_obj) {
+    super(worker_container, worker_obj);
     this.ability_name = "Pay";
     this.stat_value = this.create_stat(
       this.constructor.name,
@@ -204,21 +254,23 @@ class Income extends Stat {
 }
 
 class Happiness extends Stat {
-  constructor(worker_container) {
-    super(worker_container);
+  constructor(worker_container, worker_obj) {
+    super(worker_container, worker_obj);
     this.ability_name = "Praise";
     this.stat_value = this.create_stat(
       this.constructor.name,
       this.ability_name
     );
 
-    this.createTickListener();
+    this.new_interval = 10;
+
+    this.createTickListener(this.new_interval);
   }
 }
 
 class TestStat extends Stat {
-  constructor(worker_container) {
-    super(worker_container);
+  constructor(worker_container, worker_obj) {
+    super(worker_container, worker_obj);
     this.ability_name = "Test";
     this.stat_value = this.create_stat(
       this.constructor.name,
@@ -255,6 +307,10 @@ function decreaseStat(stat_value) {
 }
 
 // === Helper Functions === //
+
+function fetchClassEleInID(ele1_id, ele2_class) {
+  return document.getElementById(ele1_id).getElementsByClassName(ele2_class)[0];
+}
 
 function isInRange(value) {
   //return if value between min and max
